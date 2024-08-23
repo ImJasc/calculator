8142060edcbf8540f7b6f12bef926a1928f1dee2
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-22 17:44:18 -0700

    added the nodes that we will work with, modify the id of the operators buttons to have the same in all of them

--- scrypt.js
+++ scrypt.js
@@ -1,4 +1,13 @@
 let total = 0, numberOne, numberTwo = null;
+const btnNumbers = document.getElementById("number");
+const dspInput = document.getElementById("input");
+const dspOutput = document.getElementById("output");
+const btnClear = document.getElementById("clear");
+const btnErase = document.getElementById("erase");
+const btnPercent = document.getElementById("percent");
+const btnOperation = document.getElementById("operation");
+const btnDecimal = document.getElementById("decimal");
+const btnEqual = document.getElementById("equal");
 function operate (operator, numOne, numTwo){
     switch (operator){
         case '*':
@@ -16,3 +25,4 @@ function operate (operator, numOne, numTwo){
     }
     return total;
 }
+
