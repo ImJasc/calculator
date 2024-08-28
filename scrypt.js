bbe65e51393b8676173ff971147d5d58b8cfb550
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-27 19:59:38 -0700

    deleted some ifs that weren't necessary anymore. Edited the percentage eventlistener to work propperly

--- scrypt.js
+++ scrypt.js
@@ -10,18 +10,22 @@ const btnDecimal = document.querySelector("button#decimal");
 const btnEqual = document.querySelector("button#equals");
 
 btnNumbers.forEach((button) => button.addEventListener("click", recordNumber => {
-    if (dspInput.innerText === '0') {
-        dspInput.innerText = '';
-        dspInput.innerText += button.innerText;
-    }else{
-        dspInput.innerText += button.innerText;
-    }
+    if (dspOutput.innerText !== 'ERROR!') {
+        if (dspInput.innerText === '0') {
+            dspInput.innerText = '';
+            dspInput.innerText += button.innerText;
+        }else{
+            dspInput.innerText += button.innerText;
+        }   
+    }    
 }));
 
 btnDecimal.addEventListener("click", recordDecimal =>{
-    if(isDecimal === false){
-        isDecimal = true;
-        dspInput.innerText += btnDecimal.innerText;        
+    if (dspOutput.innerText !== 'ERROR!') {
+        if(isDecimal === false){
+            isDecimal = true;
+            dspInput.innerText += btnDecimal.innerText;        
+        }
     }
 });
 
@@ -37,94 +41,96 @@ btnClear.addEventListener("click", cleanScreen =>{
 });
 
 btnErase.addEventListener("click", eraseCharacter =>{    
-    if (dspInput.textContent[dspInput.textContent.length-1] === '.') {
-        isDecimal = false;
-        dspInput.innerText = dspInput.innerText.slice(0, -1);
-    }else if (dspInput.textContent.length === 1){
-        dspInput.innerText = '0';
-    }else{
-        dspInput.innerText = dspInput.innerText.slice(0, -1);
-    } 
-        
+    if (dspOutput.innerText !== 'ERROR!') {
+        if (dspInput.textContent[dspInput.textContent.length-1] === '.') {
+            isDecimal = false;
+            dspInput.innerText = dspInput.innerText.slice(0, -1);
+        }else if (dspInput.textContent.length === 1){
+            dspInput.innerText = '0';
+        }else{
+            dspInput.innerText = dspInput.innerText.slice(0, -1);
+        } 
+    }
 });
 
 btnPercent.addEventListener("click", opePercent =>{
-    if (numberOne === null) {
-        numberOne = Number(dspInput.innerText)/100;
-        dspOutput.innerText = numberOne;
-        dspInput.innerText = '';
-    }else if (numberOne !== null && operator !== null && numberTwo !== null){
-        percentage(numberOne, operator, numberTwo);
-    }else if (numberOne !== null && operator !== null && numberTwo === null){
-        numberTwo = Number(dspInput.innerText);
-        percentage(numberOne, operator, numberTwo);
+    if (dspOutput.innerText !== 'ERROR!') {
+        if (numberOne === null) {
+            numberOne = Number(dspInput.innerText)/100;
+            dspOutput.innerText = numberOne;
+            dspInput.innerText = '';
+        }else if (numberOne !== null && operator !== null){
+            numberTwo = Number(dspInput.innerText);
+            percentage(numberOne, operator, numberTwo);
+            dspOutput.innerText = numberOne;
+            dspInput.innerText = '';
+            operator = null;
+        }
     }    
 });
 
 btnOperation.forEach((button) => button.addEventListener("click", getOperator => {
-    if(operator === null){
-        numberTwo = null;        
-        operator = button.innerText;
-        final = false;
-        Equal();
-    }else{
-        numberTwo = null;
-        final = false;
-        Equal()
-        operator = button.innerText; 
-        dspOutput.innerText = numberOne + ' ' + operator;       
+    if (dspOutput.innerText !== 'ERROR!') {
+        if(operator === null){
+            numberTwo = null;        
+            operator = button.innerText;
+            final = false;
+            Equal();
+        }else{
+            numberTwo = null;
+            final = false;
+            Equal()
+            operator = button.innerText; 
+            dspOutput.innerText = numberOne + ' ' + operator;       
+        }
     }
 }));
 
 btnEqual.addEventListener("click", getequal =>{
-    final = true;
-    if (dspInput.innerText !== '') {
-        numberTwo = Number(dspInput.innerText);   
-    }    
-    Equal();
+    if (dspOutput.innerText !== 'ERROR!') {
+        final = true;
+        if (dspInput.innerText !== '' || dspOutput.innerText !== 'ERROR!') {
+            numberTwo = Number(dspInput.innerText);   
+        }    
+        Equal();
+    }
 });
 
-function Equal() {
-    if (numberOne !== null && operator !== null && numberTwo !== null) {
-        operate(numberOne, operator, numberTwo);  
-
-        if (final === true) {
-            dspOutput.innerText = numberOne;    
-            dspInput.innerText = '';
-        }else{
-            dspOutput.innerText = numberOne;
-            dspInput.innerText = '0';   
-        }        
-    }else if (numberOne !== null && operator !== null && numberTwo === null && dspInput !== '0') {
+function Equal() {   
+    if (numberOne !== null && operator !== null && dspInput !== '0') {
         numberTwo = dspInput.innerText;
         operate(numberOne, operator, numberTwo);
-
-        if (final === true) {
-            dspOutput.innerText = numberOne;
-            dspInput.innerText = '';    
-        }else{
-            dspOutput.innerText = numberOne + ' ' + operator;
-            dspInput.innerText = '0';   
-        }        
-    }else if (numberOne !== null && numberTwo === null && dspInput ==='0'){        
-        if (final === true) {
-            dspOutput.innerText = numberOne;    
-            dspInput.innerText = '';
-        }else{
-            dspOutput.innerText = numberOne + ' ' + operator;
-            dspInput.innerText = '0';   
-        }        
+        if (dspOutput.innerText !== 'ERROR!') {
+            if (final === true) {
+                dspOutput.innerText = numberOne;
+                dspInput.innerText = '';    
+            }else{
+                dspOutput.innerText = numberOne + ' ' + operator;
+                dspInput.innerText = '0';   
+            } 
+        }              
+    }else if (numberOne !== null && operator !== null && dspInput ==='0'){        
+        if (dspOutput.innerText !== 'ERROR!') {
+            if (final === true) {
+                dspOutput.innerText = numberOne;    
+                dspInput.innerText = '';
+            }else{
+                dspOutput.innerText = numberOne + ' ' + operator;
+                dspInput.innerText = '0';   
+            } 
+        }              
     }else if(numberOne === null){
-        numberOne = Number(dspInput.innerText);
+        if (dspOutput.innerText !== 'ERROR!') {
+            numberOne = Number(dspInput.innerText);
 
-        if (final === true) {
-            dspOutput.innerText = numberOne;    
-            dspInput.innerText = '';
-        }else{
-            dspOutput.innerText = numberOne + ' ' + operator;
-            dspInput.innerText = '0';   
-        }                
-        
+            if (final === true) {
+                dspOutput.innerText = numberOne;    
+                dspInput.innerText = '';
+            }else{
+                dspOutput.innerText = numberOne + ' ' + operator;
+                dspInput.innerText = '0';   
+            }    
+        }
     }
 }
 
@@ -132,19 +138,24 @@ function Equal() {
 function percentage (numOne, operator, numTwo){
     switch (operator){
         case 'x':
-            total = numOne * (numTwo/100);
+            numberOne = numOne * (numTwo/100);
             break;
         case '÷':
-            total = numOne / (numTwo/100);
+            if (Number(numTwo) === 0) {
+                dspOutput.innerText = numberOne;                
+                numTwo = null;
+                operator = null;                
+            }else{
+                numberOne = numOne / (numTwo/100);
+            }
             break;
         case '+':
-            total = numOne + (numTwo * numOne) / 100 ;
+            numberOne = numOne + (numTwo * numOne) / 100 ;
             break;
         case '-':
-            total = numOne - (numTwo * numOne) / 100;
+            numberOne = numOne - (numTwo * numOne) / 100;
             break;
     }
-    return total;
 }
 
 function operate (numOne, ope, numTwo){
@@ -153,14 +164,14 @@ function operate (numOne, ope, numTwo){
             numberOne = Number(numOne) * Number(numTwo);                                          
         break;
         case '÷':
-            if (numTwo !== 0) {
-                numberOne = Number(numOne) / Number(numTwo);                
-            }else{
+            if (Number(numTwo) === 0) {
                 dspOutput.innerText = "ERROR!";
                 numOne = null;
                 numTwo = null;
                 operator = null;
                 dspInput.innerText = '';
+            }else{                
+                numberOne = Number(numOne) / Number(numTwo);                
             }
         break;
         case '+':
